4f02c5d28fb8811fdfe4c96a0e9839b8f7755758
zedsay <59710917+zedsayhatsave@users.noreply.github.com>
2023-04-16 17:12:35 -0400

    .

---
+++ scripts/flow1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+where wget || true
+where curl || true
+where chrome || true
+where goole-chrome || true
+where firefox || true
+where edge || true
+where lynx || true
+where links || true
+where w3m || true
+ 
