9e32be7287de867a91ec94480a91bf00c372c93c
zedsay <59710917+zedsayhatsave@users.noreply.github.com>
2023-04-16 22:24:33 -0400

    .

---
+++ scripts/flow1.py
@@ -0,0 +1,37 @@
+import os
+import bae64
+
+from selenium import webdriver
+
+def chrome_driver():
+    driver = webdriver.Chrome()
+
+    # base64.b64encode('<url>'.encode('utf-8')).decode()
+    wd_download(driver, decode64('aHR0cHM6Ly93d3cubWFya2V0d2F0Y2guY29tL2ludmVzdGluZy9pbmRleC9kamlh'), 'mw_djia')
+
+    driver.quit()
+
+
+def decode64(s):
+    return base64.b64decode(s).decode('utf-8')
+
+
+def wd_download(driver, url, output_file, remove_script=True):
+    driver.get("url")
+    
+    if remove_script:
+        #element = driver.execute_script("return document.querySelector('h1')")
+        #driver.execute_script("arguments[0].setAttribute('style', 'color: red')", element)
+        driver.execute_script("""
+            var elements = document.querySelectorAll("script");
+            for (var i = 0; i < elements.length; i++) {
+                 elements[i].remove();
+            }
+            """)
+        
+    with open("page_source.html", "w", encoding="utf-8") as f:
+        f.write(driver.page_source)
+
+
+if __name__ == '__main__':
+    chrom_driver()
